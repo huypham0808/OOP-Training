61a4e5f912194ee8566c47091c6293af1c55f123
huypham0808 <phamngohoanghuy95@gmail.com>
2024-01-01 21:10:00 +0700

    Abstract class

---
+++ OOPVirtualClass/OOPAbstractClass/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAbstractClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.speed = 200;
+            car.go();
+            Console.WriteLine(((car.speed).ToString()));
+
+            Console.ReadKey();
+        }
+    }
+    abstract class Vihecle
+    {
+        public int speed = 0;
+
+        public void go ()
+        {
+            Console.WriteLine("Vihecle is moving");
+        }
+    }
+    class Car : Vihecle
+    {
+        int maxSpeed = 400;
+        public int wheels = 4;
+    }
+    class Bike: Vihecle
+    {
+        int maxSpeed = 100;
+        public int wheels = 2;
+    }
+       
+}
